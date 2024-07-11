dc9fe8f7ea5dcb37ce77f7e9138142a73446408f
Venus Nguyen <89676581+Vbeany@users.noreply.github.com>
2024-07-11 14:30:00 -0400

    Add Link to Profile Screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,6 @@
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -10,6 +12,8 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.jsx to edit this screen.</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
