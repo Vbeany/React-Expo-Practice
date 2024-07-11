cd87deee8936f4f8c89e7cb9496c590f23999580
Venus Nguyen <89676581+Vbeany@users.noreply.github.com>
2024-07-11 14:37:40 -0400

    Installed Nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -4,16 +4,10 @@ import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.jsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
+      <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
